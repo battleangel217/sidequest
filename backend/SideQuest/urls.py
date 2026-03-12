4766c0dca579b1e2669c59d7f2393f251a04161d
battleangel217 <idaraobong05@gmail.com>
2026-03-12 22:50:59 +0100

    added the google and community urls

--- backend/SideQuest/urls.py
+++ backend/SideQuest/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include([
         path('auth/', include('djoser.urls')),
-        path('auth/', include('djoser.urls.jwt'))
+        path('auth/', include('djoser.urls.jwt')),
+        path('auth/', include('users.urls')),
+        path('communities/', include('community.urls'))
     ]))
 ]
