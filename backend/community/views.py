ffd75dae5150e92882ff32e83c52b98c2c63f229
battleangel217 <idaraobong05@gmail.com>
2026-03-16 21:20:28 +0100

    Added the community detail views

--- backend/community/views.py
+++ backend/community/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -12,25 +12,35 @@ from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
 
 class CommunityViews(APIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     def get(self, request):
         # Allow user to see public communities OR communities they are a member of
         data = CommunityModel.objects.filter(Q(is_private=False) | Q(members=request.user)).distinct()
-        serializer = CommunitySerializer(data, many=True)
+        serializer = CommunitySerializer(data, many=True, context={'request': request})
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         data = request.data
-        serializer = CommunitySerializer(data=data)
+        serializer = CommunitySerializer(data=data, context={'request': request})
 
         if serializer.is_valid():
-            community_instance = serializer.save()
+            community_instance = serializer.save(created_by=request.user)
             MembershipModel.objects.create(
                 user=request.user,
                 community=community_instance,
                 role="admin"
             )
-
-            return Response({"message":"Community created successfully"}, status=status.HTTP_201_CREATED)
+            
+            # Re-serialize to get the membership data (including rank)
+            return Response(CommunitySerializer(community_instance, context={'request': request}).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommunityDetailsViews(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request, community_id):
+        data = get_object_or_404(CommunityModel, id=community_id)
+        serializer = CommunitySerializer(data, context={'request': request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
