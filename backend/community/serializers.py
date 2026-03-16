da820a5d9fb2703d6e774a5951d4e0e206d8f87f
battleangel217 <idaraobong05@gmail.com>
2026-03-16 21:19:45 +0100

    Returned members as part of the response

--- backend/community/serializers.py
+++ backend/community/serializers.py
@@ -1,9 +1,39 @@
 from rest_framework import serializers
-from .models import CommunityModel
+from .models import CommunityModel, MembershipModel
+from users.serializers import CustomUserSerializer
+
+
+class MembershipSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer(read_only=True)
+
+    class Meta:
+        model = MembershipModel
+        fields = ['user', 'role', 'community_exp', 'weekly_exp', 'joined_at']
 
 
 class CommunitySerializer(serializers.ModelSerializer):
+    # Use the related name for the reverse relationship from CommunityModel to MembershipModel
+    members = serializers.SerializerMethodField()
+    admin_id = serializers.ReadOnlyField(source='created_by.id')
+    user_rank = serializers.SerializerMethodField()
+
     class Meta:
         model = CommunityModel
-        fields = "__all__"
-        read_only_fields = ("id", "created_at", "members")
+        fields = ["id", "name", "description", "is_private", "created_at", "created_by", "admin_id", "members", "user_rank"]
+        read_only_fields = ("id", "created_at", "members", "admin_id", "created_by", "user_rank")
+
+    def get_members(self, obj):
+        memberships = obj.membershipmodel_set.all().order_by('-community_exp')
+        return MembershipSerializer(memberships, many=True).data
+
+    def get_user_rank(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            try:
+                membership = obj.membershipmodel_set.get(user=request.user)
+                # Count how many members have more exp than the current user
+                rank = obj.membershipmodel_set.filter(community_exp__gt=membership.community_exp).count() + 1
+                return rank
+            except MembershipModel.DoesNotExist:
+                return None
+        return None
