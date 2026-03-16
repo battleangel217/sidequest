89db08fd16ec830860f91bb0d0d4d6e98dea48a9
battleangel217 <idaraobong05@gmail.com>
2026-03-16 21:45:05 +0100

    Updated to postgresql

--- backend/SideQuest/settings.py
+++ backend/SideQuest/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import dotenv
 import os
+import dj_database_url
 
 dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -97,10 +98,12 @@ WSGI_APPLICATION = 'SideQuest.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=60,  # Shorter for free tier
+            conn_health_checks=True,  # Automatically reconnect if connection dies
+        )
+    
 }
 
 
