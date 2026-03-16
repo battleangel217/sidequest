20288fa7b31e7b6e9c00b22ea053ac417161e095
battleangel217 <idaraobong05@gmail.com>
2026-03-16 21:21:12 +0100

    Added the community detail urls

--- backend/community/urls.py
+++ backend/community/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CommunityViews
+from .views import CommunityViews, CommunityDetailsViews
 
 urlpatterns = [
-    path('', CommunityViews.as_view(), name="communities")
+    path('', CommunityViews.as_view(), name="communities"),
+    path('<str:community_id>/', CommunityDetailsViews.as_view(), name="community-details")
 ]
